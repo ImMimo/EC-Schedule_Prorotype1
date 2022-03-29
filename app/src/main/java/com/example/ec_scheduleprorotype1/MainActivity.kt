56e644c3c1c1b6552b88929629989ec0218c3144
Maiken Mie Scheffmann <m.scheffmann@hotmail.com>
2022-03-29 09:29:12 +0200

    Commit testing

--- app/src/main/java/com/example/ec_scheduleprorotype1/MainActivity.kt
+++ app/src/main/java/com/example/ec_scheduleprorotype1/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.ec_scheduleprorotype1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Test comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
